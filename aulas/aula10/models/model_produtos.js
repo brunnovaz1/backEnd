8a8df19e021a524a68120d5570369a48a0eaf6fd
brunnovaz1 <brunnovaz@gmail.com>
2024-05-07 21:50:00 -0300

    aula10

---
+++ aulas/aula10/models/model_produtos.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+const produtosSchema = new mongoose.Schema({
+    nome: { type: String, trim: true, uppercase:true, require:true },
+    preco: { type: Number, min:0 }
+})
+
+
+module.exports = mongoose.model("Produto", produtosSchema);
