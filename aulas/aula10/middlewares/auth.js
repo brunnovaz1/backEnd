666b352685c1545fb10588f71de0d6f97ca2b527
brunnovaz1 <brunnovaz@gmail.com>
2024-05-24 21:48:33 -0300

    jwt - .env aulas10 e 11

--- aulas/aula10/middlewares/auth.js
+++ aulas/aula10/middlewares/auth.js
@@ -1,12 +1,19 @@
-function validarToken(req, res, next) {
-    
-        if(req.headers.token === '1234567890') {
-        next()
-    } else {
-        res.status(401).json({msg: 'acesso negado'})
+    const jwt = require('jsonwebtoken');
+    function validarToken(req, res, next) {
+        const token = req.headers["authorization"]
+        if(token) {
+            try{
+                const payload = jwt.verify(token, process.env.SEGREDO);
+                console.log(payload)
+                next()
+            } catch(err) {
+                res.status(400).json({msg: 'token errado!'})     
+            }
+        } else {
+        res.status(400).json({msg: 'token inválido!'})
+        }
     }
-}
-    
+
 
 
 module.exports = validarToken;
